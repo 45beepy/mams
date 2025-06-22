490665f50f257b93f649d2dac32c023a17887e94
Pravin <pravin.bp.art@gmail.com>
2025-06-22 17:18:39 +0530

    removed shield

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -21,7 +21,7 @@ export default function LoginPage() {
   return (
     <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <ShieldCheckIcon className="mx-auto h-12 w-auto text-indigo-600" />
+    
         <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
           Military Asset Management System
         </h2>
